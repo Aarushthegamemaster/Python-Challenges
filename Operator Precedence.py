14fa6c0328dcedd70ae06fcc934747a31dd389f7
Aarushthegamemaster <aarushchandra0604@gmail.com>
2025-04-12 18:19:15 -0700

    Cool_Stuff_.py

---
+++ Operator Precedence.py
@@ -0,0 +1,14 @@
+a = 9
+b = 5
+c = 4
+d = 2
+e = 1
+f = (a-b)**d/(e + c) 
+print("(a-b)**d/(e + f) is", f)
+
+a1 = "Alex"
+a2 = 3
+if a1 == "Alex" or a1 == "Jeffrey" and a2 >=6:
+    print("Hello There!")
+else:
+    print("Bye Bye!")
