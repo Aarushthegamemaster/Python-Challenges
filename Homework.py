d46cc16c19aed2fca7fc13de2ce74d1451a3853c
Aarushthegamemaster <aarushchandra0604@gmail.com>
2025-04-13 10:59:59 -0700

    Homwwork_is_chill

---
+++ Homework.py
@@ -0,0 +1,6 @@
+a = int(input("Please enter the first number:"))
+b = int(input("Please enter the second number:"))
+c = int(input("Please enter the third number:"))
+print("Please wait for the numbers to be swapped")
+d = a,b,c = c,b,a
+print("The swapped numbers are:", d)
